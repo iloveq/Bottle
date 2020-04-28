41e736d5e1d9ca67f7b1bbb137aecb6e44608555
haoran <woaigmz@gmail.com>
2020-04-28 14:04:50 +0800

    小程序 ++

---
+++ pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+
+	</view>
+</template>
+
+<script>
+
+	export default {
+		data() {
+			return {
+
+			}
+		},
+	
+		onLoad() {
+
+		},
+		
+		mounted() {
+			
+		},
+		methods:{
+
+		}
+
+	}
+</script>
+
+<style>
+</style>
